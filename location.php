d6a80703ebb7f0defe8d30f659f6742b8464518f
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-11-14 03:55:41 -0800

    updated Location

--- location.php
+++ location.php
@@ -1,21 +1,32 @@
 <?php
 
-
+echo " time: ".locationTime('THH20','SCA30');
 //returns approximate time
 //takes in a building string (example: "VKC305")
 //returns -1 if Building not found
+/*
+|---|---|---|
+|	|  1|   |
+|---|---|---|
+|	|  2|	|
+|---|---|---|
+|  4|  3|  5|
+|---|---|---|
+
+*/
 function locationTime($building_1, $building_2){
 	//creates acronym out of the string
 	$start=preg_replace("/[^a-zA-Z]+/", "", $building_1);
 	$end=preg_replace("/[^a-zA-Z]+/", "", $building_2);
 
-	
+	echo "start: ".$start." end: ".$end;
 	$humanities=array('1','SHC','URC','FWH','UUC','LUC','CSS','MUS',
-		'BMH','CTV','SSS','RHM','CTC','LPB','HOH');
+		'BMH','CTV','SSS','RHM','CTC','LPB','SCA');
 	$vkc = array('2','VKC','SOS','WPH','CAS','THH');
-	$parkside=array('4','SGM','GFS','HNB','KAP','MCB','CWT','CWO','HRC','DRB','OHE',
-		'BHE','VHE','HED','PCE','RTH','SSC','SSL','EEB','SAL','PHE','SCA');
+	
 	$mhp=array('3','RRB','LIS','OCW','LHI','SHS','CEM','SCI','HAR','MHP');
+	$parkside=array('4','SGM','GFS','HNB','KAP','MCB','CWT','CWO','HRC','DRB','OHE',
+		'BHE','VHE','HED','PCE','RTH','SSC','SSL','EEB','SAL','PHE');
 	$law=array('5','AHF','ACC','BRI','HOH','LAW','RGL','SAS','JKP');
 
 	$map=array($humanities,$vkc,$parkside,$mhp,$law);
@@ -42,12 +53,14 @@ function locationTime($building_1, $building_2){
 	}
 
 	$dif=abs($start-$end);
-
-	if($dif<=1){
+	if($dif==0){
+		return 2;
+	}
+	if($dif==1){
 		return 3;
 	}
 	else if($dif==2){
-		return 6;
+		return 5;
 	}
 	else{
 		return 10;
