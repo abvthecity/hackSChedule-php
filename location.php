e568b61f744a1b849751b1eb718b8b9203eb35dd
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-11-14 03:36:14 -0800

    time estimator based on location

--- location.php
+++ location.php
@@ -1,35 +1,58 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "johncena";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {die("DUMP");}
-//find all buildings
-$result = mysql_query("SELECT id, room FROM section");
-
-function createBuildingArray(){
-	$building = array(); 
-	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    //printf("ID: %s  Room: %s", $row["id"], $row["room"]);
-		$building = $row["room"];
-	}
-	$unique = array();
-	for($i=0;$i<sizeof($building);$i++){
-		if(!in_array($building[$i],$unique)){
-			$unique[]=$building[$i];
-		}
-	}
-	return $unique;
-}
-
 
 //returns approximate time
-//takes in a building string (example: "BUAD-305")
+//takes in a building string (example: "VKC305")
+//returns -1 if Building not found
 function locationTime($building_1, $building_2){
-	//extracts acronym from string 
+	//creates acronym out of the string
+	$start=preg_replace("/[^a-zA-Z]+/", "", $building_1);
+	$end=preg_replace("/[^a-zA-Z]+/", "", $building_2);
+
 	
+	$humanities=array('1','SHC','URC','FWH','UUC','LUC','CSS','MUS',
+		'BMH','CTV','SSS','RHM','CTC','LPB','HOH');
+	$vkc = array('2','VKC','SOS','WPH','CAS','THH');
+	$parkside=array('4','SGM','GFS','HNB','KAP','MCB','CWT','CWO','HRC','DRB','OHE',
+		'BHE','VHE','HED','PCE','RTH','SSC','SSL','EEB','SAL','PHE','SCA');
+	$mhp=array('3','RRB','LIS','OCW','LHI','SHS','CEM','SCI','HAR','MHP');
+	$law=array('5','AHF','ACC','BRI','HOH','LAW','RGL','SAS','JKP');
+
+	$map=array($humanities,$vkc,$parkside,$mhp,$law);
+	for($a=0;$a<sizeof($map);$a++){
+		for($b=0;$b<sizeof($map[$a]);$b++){
+			if($map[$a][$b]==$start){
+				$start=$map[$a][0];
+			}
+			if($map[$a][$b]==$end){
+				$end=$map[$a][0];
+			}
+		}
+	}
+	$start=intval($start);
+	$end=intval($end);
+
+	if($start ==0 || $end ==0){
+		return -1;
+	}
+
+
+	if(($start==4 || $end == 4)&&($start == 5 || $end == 5)){
+		return 6;
+	}
+
+	$dif=abs($start-$end);
+
+	if($dif<=1){
+		return 3;
+	}
+	else if($dif==2){
+		return 6;
+	}
+	else{
+		return 10;
+	}
+
 }
 
 ?>
